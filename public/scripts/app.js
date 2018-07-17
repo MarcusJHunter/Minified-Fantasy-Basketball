2c1b74139e57c6780d0425b9c2be8aa58638190e
bjimison <brady.t.jimison@gmail.com>
2018-07-17 09:10:41 -0700

    finalized folder layout & initiated/required appropriate node packages on designated .js files

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+
+const. MySportsFeeds = require("mysportsfeeds-node");
+
+const msf = new MySportsFeeds("1.0", true);
+msf.authenticate("bjimison", "Bjsoc.999");
+
+const apiKey = "e2d646dd-b136-40e8-8f5a-68053b";
+
